e3bb3402f195e6686596fc020cd789cab48f1297
helal9380 <asadurrahman9380@gmail.com>
2024-10-06 17:21:49 +0600

    added the origin

--- index.js
+++ index.js
@@ -3,16 +3,19 @@
 const express = require("express");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const cors = require("cors");
-require('dotenv').config()
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://vercel.com/helal9380s-projects",
+  })
+);
 app.use(express.json());
 
-const uri =
-  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.0qkhitp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.0qkhitp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -25,9 +28,10 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-  
     const artCollection = client.db("artCraftDB").collection("artCraft");
-    const subcategoryCollection = client.db("artCraftDB").collection("subcategories");
+    const subcategoryCollection = client
+      .db("artCraftDB")
+      .collection("subcategories");
     app.get("/addArt", async (req, res) => {
       const cursor = artCollection.find();
       const result = await cursor.toArray();
@@ -40,19 +44,19 @@ async function run() {
       res.send(result);
     });
 
-    app.get("/category", async(req,res) => {
+    app.get("/category", async (req, res) => {
       const cursor = artCollection.find();
       const result = await cursor.toArray();
-      res.send(result)
-    })
+      res.send(result);
+    });
 
-    app.get('/categorydetails/:id', async(req,res) => {
+    app.get("/categorydetails/:id", async (req, res) => {
       const id = req.params.id;
       console.log(id);
-      const query = {_id : new ObjectId(id)};
+      const query = { _id: new ObjectId(id) };
       const result = await artCollection.findOne(query);
-      res.send(result)
-    })
+      res.send(result);
+    });
 
     app.get("/category/:id", async (req, res) => {
       const subCategory = req.params.id;
@@ -99,24 +103,21 @@ async function run() {
           stock: art.stock,
         },
       };
-      const result = await artCollection.updateOne(filter,udpadedArt,options);
-      res.send(result)
+      const result = await artCollection.updateOne(filter, udpadedArt, options);
+      res.send(result);
     });
 
-    app.delete('/myArt/:id', async(req,res) => {
+    app.delete("/myArt/:id", async (req, res) => {
       const id = req.params.id;
-      const query = {_id: new ObjectId(id)};
+      const query = { _id: new ObjectId(id) };
       const result = await artCollection.deleteOne(query);
-      res.send(result)
-    })
+      res.send(result);
+    });
 
-   
-   
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
   } finally {
-    
   }
 }
 run().catch(console.dir);
